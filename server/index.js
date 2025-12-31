6eb1a868a7517f9cc591a4caa835784842042a2c
Hpyopp <168950048+Hpyopp@users.noreply.github.com>
2025-12-31 23:30:34 +0530

    FORCE UPDATE: Fixed login routes and index

--- server/index.js
+++ server/index.js
@@ -3,21 +3,22 @@ const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const path = require('path'); 
-const fs = require('fs'); // 👈 IMPORT FS MODULE
-const cors = require('cors');
+const fs = require('fs');
+const cors = require('cors'); // Ab ye error nahi dega kyunki tune Step 1 kar liya
 
 const port = process.env.PORT || 5000;
 
+// Database Connect
 connectDB();
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(cors()); 
 
-// 👇 AUTO-CREATE UPLOADS FOLDER
-// Ye code check karega ki folder hai ya nahi. Nahi toh bana dega.
+// Auto-Create Uploads Folder (Crash Proof)
 const uploadDir = path.join(__dirname, 'uploads');
 if (!fs.existsSync(uploadDir)){
     fs.mkdirSync(uploadDir);
@@ -28,9 +29,10 @@ if (!fs.existsSync(uploadDir)){
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/events', require('./routes/eventRoutes'));
 
-// Folder ko Public karo
+// Public Folder
 app.use('/uploads', express.static(uploadDir));
 
+// Error Handler
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
